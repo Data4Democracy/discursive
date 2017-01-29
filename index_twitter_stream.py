23e31afb091f46f5b8e5285f61a15a7e60a7331c
Ahmad Ragab <ASRagab@Ragab-MBP.local>
2017-01-28 23:45:17 -0500

    Issues around config and sys args adding dockerignore

--- index_twitter_stream.py
+++ index_twitter_stream.py
@@ -4,6 +4,7 @@ from config import esconn, aws_config, twitter_config
 import os
 from datetime import datetime as dt
 from tweet_model import map_tweet_for_es
+from config import s3conn
 
 # unicode mgmt
 import sys
@@ -18,8 +19,8 @@ api = tweepy.API(auth)
 # Get elasticsearch connection
 es = esconn.esconn()
 
-if len(sys.argv) >= 2:
-    sys.exit('ERROR: Received 2 or more arguments. Expected 1: Topic file name')
+if len(sys.argv) > 2:
+    sys.exit('ERROR: Received 2 or more arguments: {} {} {} Expected 1: Topic file name'.format(sys.argv[0], sys.argv[1], sys.argv[2]))
 
 elif len(sys.argv) == 2:
     try:
