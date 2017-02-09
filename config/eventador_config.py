0ad08589491b32b4c9aab91dd259e9a6520016a3
Ahmad Ragab <ASRagab@Ragab-MBP.local>
2017-02-08 22:08:19 -0500

    cleanup

--- config/eventador_config.py
+++ config/eventador_config.py
@@ -1,9 +1,25 @@
-brokers = ''
-
-s3config = {
-    "brokers": brokers, # Eventador plain text broker
-    "topic": 'tweets',  # Eventador (Kafka Topics)
-    "bucket": 'testdiscursive', # S3 Bucket
-    "max_records": 10, # max records to stream from twitter
-    "publisher": 's3'  # some kind of backend publisher i.e. s3, elasticsearch, etc
+config = {
+    "brokers": '',  # Eventador plain text broker
+    "topic": 'tweets',   # Eventador (Kafka Topics)
+    "max_records": 10,   # max records to stream from twitter
 }
+
+# S3 Config
+def add_publisher_config(common, config):
+    c = common.copy()
+    c.update(config)
+    return c
+
+
+s3_config = add_publisher_config(config, {
+    "publisher": "s3",
+    "bucket": "testdiscursive",  # S3 Bucket
+})
+
+
+# Elasticsearch Config
+es_config = add_publisher_config(config, {
+    "publisher": "es",
+    "index": "twitter",
+    "doc_type": "tweets"
+})
